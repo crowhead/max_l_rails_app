6f4687530878dea635845583219b6f6ee02ebf94
crowhead <maxlewis0@gmail.com>
2014-10-28 14:29:38 -0400

    Moccasin App: created migration that adds column for timestamps to Users model
    Created a migration that adds a column to the Users Table for timestamps "created at" and "updated at"

--- moccasin/db/schema.rb
+++ moccasin/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141028173319) do
+ActiveRecord::Schema.define(version: 20141028175826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,11 +24,13 @@ ActiveRecord::Schema.define(version: 20141028173319) do
   end
 
   create_table "users", force: true do |t|
-    t.string  "name"
-    t.string  "password_digest"
-    t.string  "remember_token"
-    t.boolean "admin"
-    t.string  "email"
+    t.string   "name"
+    t.string   "password_digest"
+    t.string   "remember_token"
+    t.boolean  "admin"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
