f5609fadb3d2a3bc9102b3234a64abb30266dd6d
crowhead <maxlewis0@gmail.com>
2014-10-28 14:40:24 -0400

    Moccasin App: generated migration to add index to Users for email, remember token
    Added index for email uniqueness: true and remember_token to Users

--- moccasin/db/migrate/20141028175826_add_timestamps_to_users.rb
+++ moccasin/db/migrate/20141028175826_add_timestamps_to_users.rb
@@ -3,4 +3,5 @@ class AddTimestampsToUsers < ActiveRecord::Migration
     add_column :users, :created_at, :datetime
     add_column :users, :updated_at, :datetime
   end
+
 end
--- moccasin/db/schema.rb
+++ moccasin/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141028175826) do
+ActiveRecord::Schema.define(version: 20141028183828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,5 +32,8 @@ ActiveRecord::Schema.define(version: 20141028175826) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
