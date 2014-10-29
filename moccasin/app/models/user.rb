1301d74e8ed49241d136144f1064409228f23f29
crowhead <maxlewis0@gmail.com>
2014-10-29 10:45:37 -0400

    Moccasin app add devise for users w/ randy

--- moccasin/app/models/user.rb
+++ moccasin/app/models/user.rb
@@ -1,47 +1,7 @@
 class User < ActiveRecord::Base
-  has_many :posts
-
-before_create :create_remember_token
-before_save :normalize_fields
-
-# Validates name:
-  validates :name,
-    presence: true,
-    length: {maximum: 50}
-
-  # Validates email:
-  validates :email,
-    presence: true,
-    uniqueness: {case_sensitive: false},
-    format: {with:  /[^@]+@[^@]+/ }
-
-
-  # Validates password length
-  validates :password,
-    length: {minimum: 8}
-
-  # secure password feature
-  has_secure_password
-
-  # Create a new remember token for the user:
-  def self.new_remember_token
-      SecureRandom.urlsafe_base64
-  end
-
-  # Hash a token:
-  def self.hash(token)
-    Digest::SHA1.hexdigest(token.to_s)
-  end
-
-  private
-  # Create a new sessions token for the user
-  def create_remember_token
-    remember_token = User.hash(User.new_remember_token)
-  end
-
-  # Normalize fields for consistency
-  def normalize_fields
-    # self.email.downcase!
-  end
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
-
