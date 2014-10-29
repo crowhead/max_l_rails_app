6a45f05a0ea8cef68a80fbd2bd19c01505439b09
crowhead <maxlewis0@gmail.com>
2014-10-29 16:35:32 -0400

    Moccasin App: navigation splash page, singin, signup, signout
    Created views files for signin, signup, signout that stem from the splash/welcome page

--- moccasin/config/routes.rb
+++ moccasin/config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :activities
+  devise_for :users
 
-  get '/sign_up', to: 'users#new'
-  get '/sign_in', to: 'sessions#new', as: 'new_session'
-  delete '/signout', to: 'sessions#destroy'
+
+  devise_scope :user do
+    get '/sign_up', to: 'devise/registration#new'
+    get '/sign_in', to: 'devise/sessions#new', as: 'new_session'
+    delete '/signout', to: 'devise/sessions#destroy'
+  end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
